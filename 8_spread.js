744f38d3756a44c14a67432b9c88d6944c970cac
Arfanur <arfanur03@gmail.com>
2022-05-06 20:42:05 +0600

    6-5-22

--- 8_spread.js
+++ 8_spread.js
@@ -4,9 +4,9 @@ const numbers = [23, 65, 99, 21, 34];
 
 const max = Math.max(23, 99, 65, 21, 34);
 const maxInArray = Math.max(...numbers);
-// console.log(max, maxInArray);
+// console.log(max, maxInArray);       // 99 99
 
 const numbers2 = [...numbers, 88];
 numbers.push(55);
-console.log(numbers);
-console.log(numbers2);
+console.log(numbers);       // [ 23, 65, 99, 21, 34, 55 ]
+console.log(numbers2);      // [ 23, 65, 99, 21, 34, 88 ]
